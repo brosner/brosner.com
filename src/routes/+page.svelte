53700da60d2848b38aebc63d938e0cfea1d69f50
Brian Rosner <brian@brosner.com>
2023-07-06 23:20:46 -0600

    header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit woooo</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="section">
+  <div class="container">
+    <h1 class="title is-1">Brian Rosner</h1>
+  </div>
+</section>
