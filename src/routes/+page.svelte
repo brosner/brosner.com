6b99287f7021946fb1709b44cb5e616b0987f575
Brian Rosner <brian@brosner.com>
2023-07-06 23:32:28 -0600

    more app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<section class="section">
-  <div class="container">
-    <h1 class="title is-1">Brian Rosner</h1>
+<div id="app">
+  <div class="hello wrapper">
+    <section class="section">
+      <div class="container">
+        <h1 class="title is-1">Brian Rosner</h1>
+      </div>
+    </section>
   </div>
-</section>
+</div>
